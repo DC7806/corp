93dd31c07887212cc5e3f1bb318753a3b93e74b2
DC <dachang.h@gmail.com>
2018-02-25 08:01:32 +0800

    created admin system setting with writing to yml and vanilla file upload

---
+++ app/controllers/admin/system_controller.rb
@@ -0,0 +1,49 @@
+class Admin::SystemController < AdminController
+
+  before_action :find_system
+  require 'yaml'
+
+  def index
+    @admin_system_site_name = @admin_system.slice('site_name')
+    @admin_system_tracking = @admin_system.slice('tracking')
+    @admin_system_logo = @admin_system.slice('logo')
+  end
+
+  def create
+    # load
+    # @admin_system = YAML::load_file("#{Rails.root}/config/system.yml")
+    # write
+    data = params[:system].except('logo')
+    data.each do |k1, v1|
+      v1.each do |k2, v2|
+        @admin_system[k1][k2] = v2
+      end
+    end
+    # store
+    File.write("#{Rails.root}/config/system.yml", @admin_system.to_yaml)
+
+    @admin_system.slice('logo').values.first.keys.each do |locale|
+      upload(locale)
+    end
+
+    redirect_back(fallback_location: request.referrer)
+  end
+
+  private
+  
+  def upload(locale)
+    if params[:system][:logo] && params[:system][:logo][locale.to_sym] != nil
+      uploaded_io = params[:system][:logo][locale.to_sym]
+      logo_dir = 'public/images/system'
+      File.open(Rails.root.join(logo_dir, uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+        File.rename(file, "#{logo_dir}/logo_#{locale}.jpg")
+      end
+    end
+  end
+  
+  def find_system
+    @admin_system = YAML::load_file("#{Rails.root}/config/system.yml")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   namespace :admin, path: Settings.admin_secret_path do
     resources :news, :categories, :products, :certificates, :faqs, :milestones, except: :show
     resources :translations, only: [:index, :edit, :update]
-    resources :about, :contacts, only: [:index, :create]
+    resources :about, :contacts, :system, only: [:index, :create]
     get 'inquiries'
     # get 'dashboard', to: 'admin#dashboard', as: :root
   end
